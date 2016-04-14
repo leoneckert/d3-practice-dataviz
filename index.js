41026b8f05c68f7d43a89fd0a9753de92dd0fd82
Leon Eckert <eckertleon@gmail.com>
2016-04-13 22:00:36 -0400

    made changes for heroku

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');  // gets it from the node_modules folder
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public')); 
 
-app.listen(3000, function(){
-	console.log('Sleep Reaction app listening on port 3000!');
+// app.listen(3000, function(){
+// 	console.log('Sleep Reaction app listening on port 3000!');
+// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
